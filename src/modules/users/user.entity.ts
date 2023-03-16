c92c740422aea70503bd094c407c055ed1ae3f20
Émerson Yuri <emerson.yur@gmail.com>
2023-03-17 00:07:56 +0200

    feat: setup project + add user module

---
+++ src/modules/users/user.entity.ts
@@ -0,0 +1,74 @@
+import {
+  Table,
+  Column,
+  Model,
+  DataType,
+  AutoIncrement,
+  PrimaryKey,
+  IsUrl,
+  Default,
+  CreatedAt,
+  UpdatedAt,
+} from 'sequelize-typescript';
+
+@Table
+export class User extends Model<User> {
+  @AutoIncrement
+  @PrimaryKey
+  @Column({
+    type: DataType.INTEGER,
+    allowNull: false,
+  })
+  id: number;
+
+  @Column({
+    type: DataType.STRING,
+    allowNull: false,
+  })
+  username: string;
+
+  @Column({
+    type: DataType.STRING,
+    unique: true,
+    allowNull: false,
+  })
+  email: string;
+
+  @Column({
+    type: DataType.STRING,
+  })
+  phone: string;
+
+  @IsUrl
+  @Column
+  image: string;
+
+  @Column({
+    type: DataType.INTEGER,
+  })
+  cs: number;
+
+  @Default(2)
+  @Column({
+    type: DataType.INTEGER,
+  })
+  tests: number;
+
+  @Default(false)
+  @Column({
+    type: DataType.BOOLEAN,
+  })
+  premium: boolean;
+
+  @CreatedAt
+  @Column({
+    type: DataType.DATE,
+  })
+  createdAt: Date;
+
+  @UpdatedAt
+  @Column({
+    type: DataType.DATE,
+  })
+  updatedAt: Date;
+}
